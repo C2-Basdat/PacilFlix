7e3ba28752c5c6dcd5768a74380fc68e02be8b13
Austin <austinsusanto@gmail.com>
2024-04-30 01:09:35 +0700

    Created show_daftar_unduhan function & daftar_unduhan.html

--- daftar_unduhan/urls.py
+++ daftar_unduhan/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from daftar_unduhan.views import show_daftar_unduhan
+
+app_name = 'daftar_unduhan'
+
+urlpatterns = [
+    path('', show_daftar_unduhan, name='show_daftar_unduhan'),
+]
--- daftar_unduhan/views.py
+++ daftar_unduhan/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# @login_required(login_url='/login')
+def show_daftar_unduhan(request):
+    context = {}
+    return render(request, 'daftar_unduhan.html', context)
