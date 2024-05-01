8e3fd013094a84f5522f4feda975fd3f8d3b576a
Austin <austinsusanto@gmail.com>
2024-05-01 16:12:20 +0700

    Initialized daftar_favorit app & included in urls.py

---
+++ daftar_favorit/urls.py
@@ -0,0 +1,1 @@
+app_name = 'daftar_favorit'
---
+++ daftar_favorit/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+# @login_required(login_url = '/login')
+def show_daftar_favorit
