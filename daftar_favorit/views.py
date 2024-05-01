6fbebacbc4708b024b36e40a665ae7a5e4218f65
Austin <austinsusanto@gmail.com>
2024-05-01 16:20:54 +0700

    Created show_daftar_favorit & daftar_favorit.html

--- daftar_favorit/views.py
+++ daftar_favorit/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 # @login_required(login_url = '/login')
-def show_daftar_favorit
+def show_daftar_favorit(request):
+    context = {}
+    return render(request, 'daftar_favorit.html', context)
