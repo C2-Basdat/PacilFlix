fb0aa67f70203256cfe45d7b8414353f32132bbc
Austin <austinsusanto@gmail.com>
2024-05-01 15:48:31 +0700

    Added contents to daftar_unduhan.html

--- daftar_unduhan/views.py
+++ daftar_unduhan/views.py
@@ -3,5 +3,9 @@ from django.contrib.auth.decorators import login_required
 
 # @login_required(login_url='/login')
 def show_daftar_unduhan(request):
-    context = {}
+    daftar_unduhan_user = []
+
+    context = {
+        'daftar_unduhan_user': daftar_unduhan_user
+    }
     return render(request, 'daftar_unduhan.html', context)
