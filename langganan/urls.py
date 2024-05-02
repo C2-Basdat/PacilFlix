e5003471b5100451ae7f908ae825357d480c7c2a
bimantorow <bimantoro.widyadana@ui.ac.id>
2024-05-02 19:37:10 +0700

    finish buy page

--- langganan/urls.py
+++ langganan/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('beli/', views.beli, name='beli')
 ]
