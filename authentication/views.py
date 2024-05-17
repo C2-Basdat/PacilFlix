20c544f413786e68de6ba509e36ac4e670638ff7
Matthew <matthewhjt72@gmail.com>
2024-05-17 14:06:59 +0700

    feat: add auth api endpoints

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,9 @@
+import json
+from django.http import HttpResponseRedirect, HttpResponseNotFound, JsonResponse
 from django.shortcuts import render
+from authentication.backends import AuthBackend
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages  
 
 def show_auth(request):
     return render(request, 'auth.html')
@@ -8,3 +13,24 @@ def show_login(request):
 
 def show_register(request):
     return render(request, 'register.html')
+
+def auth_login(request):
+    if request.method == 'POST':
+        be = AuthBackend()
+        requestBody = json.load(request)
+        username = requestBody['username']
+        password = requestBody['password']
+        user = be.authenticate(username, password)
+        if user is not None:
+            be.login(request, user)
+            return JsonResponse({'message': 'Login success!'}, status=200)
+        else:
+            return JsonResponse({'message': 'Login failed! Wrong username or password.'}, status=401)
+    else:
+        return JsonResponse(status=405)
+        
+@csrf_exempt
+def auth_logout(request):
+    if request.method == 'POST':
+        be = AuthBackend()
+        be.logout(request)
