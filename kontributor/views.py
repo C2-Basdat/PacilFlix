594c6bb8093a39aaa42bbd55ae99b6cf68d4cb01
Bimantoro <bimantoro.widyadana@ui.ac.id>
2024-05-17 13:36:22 +0700

    finish contributor retrieval

--- kontributor/views.py
+++ kontributor/views.py
@@ -1,4 +1,34 @@
 from django.shortcuts import render
+from utils import DatabaseConnection
 
 def index(request):
-    return render(request, 'kontributor.html', {})
+    database = DatabaseConnection()
+    query_str = """
+    SELECT
+        nama,
+        (CASE
+            WHEN type1 IS NULL AND type2 IS NULL AND type3 IS NULL THEN 'Unknown'
+            ELSE CONCAT_WS(', ', type1, type2, type3)
+        END) as type,
+        jenis_kelamin,
+        kewarganegaraan
+    FROM (
+        SELECT
+            nama,
+            (CASE WHEN s.id IS NOT NULL THEN 'Sutradara' END) AS type1,
+            (CASE WHEN p.id IS NOT NULL THEN 'Pemain' END) AS type2,
+            (CASE WHEN ps.id IS NOT NULL THEN 'Penulis Skenario' END) AS type3,
+            (CASE WHEN c.jenis_kelamin=0 THEN 'Laki-laki' ELSE 'Perempuan' END) as jenis_kelamin,
+            kewarganegaraan
+        FROM
+            CONTRIBUTORS c
+            LEFT JOIN SUTRADARA s on c.id=s.id
+            LEFT JOIN PEMAIN p on c.id=p.id
+            LEFT JOIN PENULIS_SKENARIO ps on c.id=ps.id
+    );
+    """
+    contributors = database.query(query_str)
+    context = {
+        'contributors': contributors
+    }
+    return render(request, 'kontributor.html', context)
