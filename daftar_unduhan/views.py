71e1e0be648fb33fb2429c8456496d5e66ddbdf3
Austin <austinsusanto@gmail.com>
2024-05-17 21:14:46 +0700

    Implemented create feature for daftar_unduhan

--- daftar_unduhan/views.py
+++ daftar_unduhan/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import redirect, render
 
 from utils import DatabaseConnection
@@ -17,3 +18,24 @@ def show_daftar_unduhan(request):
     context['daftar_unduhan'] = daftar_unduhan
 
     return render(request, 'daftar_unduhan.html', context)
+
+def add_daftar_unduhan(request, id_tayangan):
+    # verify login
+    if request.session['user'] == None:
+        return redirect("/auth")
+    context = {'authenticated' : True, 'user' : request.session['user']}
+
+    username = request.session['user']['username']
+
+    database = DatabaseConnection()
+    select_query = f"SELECT * FROM TAYANGAN_TERUNDUH TT WHERE TT.username = '{username}' AND TT.id_tayangan = '{id_tayangan}'"
+    daftar_unduhan = database.query(select_query)
+
+    if not daftar_unduhan:
+        
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+        insert_query = f"INSERT INTO TAYANGAN_TERUNDUH VALUES('{id_tayangan}','{username}','{timestamp}');"
+        database.query(insert_query)
+    
+    return redirect('/fakhri-hijau/tayangan')
